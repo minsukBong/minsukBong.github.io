09165f777817cf90e89b20e92d5c1a451d04f532
minsukBong <76250057+minsukBong@users.noreply.github.com>
2020-12-20 16:08:39 +0900

    수정_1220_1608

--- js/scripts.js
+++ js/scripts.js
@@ -27,7 +27,7 @@
 
         $('html, body').animate({
             scrollTop: scrollDistance + 'px'
-        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
+        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 2);
 
         // Hide the menu once clicked if mobile
         if ($('header').hasClass('active')) {
@@ -39,7 +39,7 @@
     $('#to-top').click(function() {
         $('html, body').animate({
             scrollTop: 0
-        }, 500);
+        }, 300);
     });
 
     // Scroll to first element
@@ -47,7 +47,7 @@
         var scrollDistance = $('#lead').next().offset().top;
         $('html, body').animate({
             scrollTop: scrollDistance + 'px'
-        }, 500);
+        }, 300);
     });
 
     // Create timeline
@@ -74,6 +74,17 @@
             }
         });
 
+    });
+
+    $('a[id^=view]').on('click', function() {
+        if( $("p#view"+this.rel).css('display') == "none" )
+        {
+            $("p#view"+this.rel).css('display', 'block');
+        }
+        else
+        {
+            $("p#view"+this.rel).css('display', 'none');
+        }
     });
 
     // Open mobile menu
